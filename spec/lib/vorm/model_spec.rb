fc3b48a2ba7ecc702518b5d3e7d757b945ccac4e
Juho H <juho.hautala@helsinki.fi>
2014-10-21 15:19:42 +0300

    specs refactored to mirror mixin

--- spec/lib/vorm/model_spec.rb
+++ spec/lib/vorm/model_spec.rb
@@ -1,81 +1,9 @@
-module Vorm
-  class Model
-    def self.reset
-      @table = nil
-      @fields = nil
-    end
-  end
-end
-
 describe Vorm::Model do
   context "class methods" do
     subject { Vorm::Model }
 
-    describe ".table" do
-      before { subject.reset }
-
-      it { is_expected.to respond_to(:table) }
-
-      it "raises when name is not string" do
-        expect { subject.table(:users) }
-        .to raise_error(ArgumentError, "Table name must be a string")
-      end
-
-      it "sets the table correctly" do
-        expect { subject.table('users') }
-        .to change(subject, :table).to('users')
-      end
-
-      it "gets the table" do
-        subject.table('users')
-        expect(subject.table).to eq('users')
-      end
-
-      it "setting the table again does not change it (cached)" do
-        subject.table('users')
-        expect { subject.table('another table') }
-        .not_to change(subject, :table)
-      end
-    end
-
-    describe ".field" do
-      before { subject.reset }
-
-      it { is_expected.to respond_to(:field) }
-
-      it "raises when name is not string" do
-        expect { subject.field(:email) }
-        .to raise_error(ArgumentError, "Field name must be a string")
-      end
-
-      it "sets the field correctly" do
-        expect { subject.field('email') }
-        .to change(subject, :fields)
-      end
-
-
-      it "ignores duplicates" do
-        subject.field('email')
-        subject.field('email')
-        expect(subject.fields).to eq(Set.new(['email']))
-      end
-    end
-
-    describe ".fields" do
-      before { subject.reset }
-
-      it { is_expected.to respond_to(:fields) }
-
-      it "gets fields when only one set" do
-        subject.field('email')
-        expect(subject.fields).to eq(Set.new(['email']))
-      end
-
-      it "gets the fields when multiple set" do
-        subject.field('email')
-        subject.field('dob')
-        expect(subject.fields).to eq(Set.new(['email', 'dob']))
-      end
+    it "should include Persistable" do
+      expect(subject.included_modules).to include(Vorm::Persistable)
     end
   end
 end
